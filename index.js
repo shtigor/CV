c9e1e75f1b07b8d3cd5a5f9a405d9a923c683618
Igor Shturmak <shturmak.i.i@gmail.com>
2019-12-13 02:47:17 +0300

    feat: update template, add personal info

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-function loadJSON(callback) {   
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'locale.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
+const locale = {
+    en: {
+        welcome: "Hello everybody!"
+    },
+    ru: {
+        welcome: "Всем привет!"
+    }
 }
-
-function init() {
-    loadJSON(function(response) {
-     // Parse JSON string into object
-    //    var actual_JSON = JSON.parse(response);
-       console.log(JSON.parse(response))
-    });
-}
-
-init();
-// const locale = JSON.parse(xhr.responseText);
 
 let welcome = document.querySelector('.hi')
 
 // define language using window hash
 if (window.location.hash) {
     if (window.location.hash === "#ru") {
-        welcome.textContent = locale["ru"]["welcome"];
+        welcome.textContent = locale.ru.welcome;
     }
 }
 
