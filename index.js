0c3cc3d407f98dfd5ff72567bc732a2535adfdff
Igor <shturmak.i.i@gmail.com>
2019-12-16 10:31:32 +0300

    feat: update lamguages, add projects

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ const locale = {
         contacts: "Contacts",
         infoDescription: "Hi! My name is Igor. On this page you can see my achivements, career steps, projects. I want to become Full Stack Developer. For me learning something new really important. If I want to become a good developer I should read more books, articles. Take part in conferences to improve my skills. In future I want to relocate.",
         education: "Education",
-        educationDescription: "I've graduated from BSUIR in 2017.",
-        courses: ""
+        educationDescription: "I graduated from BSUIR in 2017.",
+        courses: "Courses",
+        englishCourses: "English courses",
+        englishCoursesDescription: "I've finished Streamline Intermediate and Upper-Intermediate course.",
+        onlineCourses: "Online courses",
+        onlineCoursesDescription: "Coursera python course (<a href=\"https://www.coursera.org/learn/diving-in-python\" target=\"_blank\">Diving in python</a>)",
+        technologiesTranslation: "Technologies",
+        projectHeaderTranslation: "Projects",
+        careerHeaderTranslation: "Career",
+        fsrtCareerDate: "February 2017 - August 2018",
+        fsrtCareerName: "«Center vnedreniy» Ltd",
+        fsrtCareerPstn: "Programmer",
+        fsrtCareerDscrptn: "Create, update configurations. Work with clients, solved their issues.",
+        scndCareerDate: "August 2018 - December 2019",
+        scndCareerName: "Nitka Technologies",
+        scndCareerPstn: "Programmer analyst",
+        scndCareerDscrptn: "Create surveys and analyze respondent data. Work in team, attend team meetings. Abilities which I've got are read other's people code, work in team, solve tasks quickly. Also I develop my English.",
     },
     ru: {
         about: "О мне",
@@ -17,17 +32,55 @@ const locale = {
         infoDescription: "Меня зовут Игорь. На данной странице мы можете увидеть мои достижения, карьерную лестницу, проекты. Я хочу стать Full Stack разработчиком. Для меня изучение чего-то нового очень важно. Я читаю книги, статьи, посещаю курсы для своего развития. Так же я считаю для получения опыта необходимо участвовать в различных конференциях для улучшения своих навыков.",
         education: "Образование",
         educationDescription: "Я закончил БГУИР в 2017 году.",
+        courses: "Курсы",
+        englishCourses: "Курсы английского языка",
+        englishCoursesDescription: "Закончил курсы английского языка в школе Стримлайн уровня Intermediate и Upper-Intermediate.",
+        onlineCourses: "Онлайн курсы",
+        onlineCoursesDescription: "Python курс на Coursera (Погружение в Python)",
+        technologiesTranslation: "Технологии",
+        projectHeaderTranslation: "Проекты",
+        careerHeaderTranslation: "Карьера",
+        fsrtCareerDate: "Февраль 2017 - Август 2018",
+        fsrtCareerName: "ООО «Цент внедрений»",
+        fsrtCareerPstn: "Инженер-программист",
+        fsrtCareerDscrptn: "Создание, обновление конфигураций. Работа с клиентами.",
+        scndCareerDate: "Август 2018 - Декабрь 2019",
+        scndCareerName: "Nitka Technologies",
+        scndCareerPstn: "Программист-аналитик",
+        scndCareerDscrptn: "Создание опросов, создание таблиц для анализа данных респондентов. Работа в команде, в митингах. На данном проекте приобрел навыки работы в команде, чтение чужого кода, решение задач быстро. Также улучшил знания английского языка.",
     }
 }
 
 let about = document.querySelector('#about-me')
 let career = document.querySelector('#career-nav')
-let projects = document.querySelector('#projects')
-let contacts = document.querySelector('#contacts')
+let projects = document.querySelector('#projects-nav')
+let contacts = document.querySelector('#contacts-nav')
 
 let headerDescription = document.querySelector('.info__about--header')
 let description = document.querySelector('#description')
-let description = document.querySelector('.education')
+let education = document.querySelector('.education')
+let educationDescription = document.querySelector('#educationDescription')
+
+let courses = document.querySelector('.courses')
+let englishCourses = document.querySelector('.english-courses')
+let englishCoursesDescription = document.querySelector('.english-courses-description')
+let onlineCourses = document.querySelector('.online-courses')
+let onlineCoursesDescription = document.querySelector('.online-courses-description')
+let technologiesTranslation = document.querySelector('.technologies-translation')
+let projectHeaderTranslation = document.querySelector('.project-header-translation')
+
+let careerHeaderTranslation = document.querySelector('.career-header-translation')
+
+let fsrtCareerDate = document.querySelector('.fsrt-career-date')
+let fsrtCareerName = document.querySelector('.fsrt-career-name')
+let fsrtCareerPstn = document.querySelector('.fsrt-career-pstn')
+let fsrtCareerDscrptn = document.querySelector('.fsrt-career-dscrptn')
+
+let scndCareerDate = document.querySelector('.scnd-career-date')
+let scndCareerName = document.querySelector('.scnd-career-name')
+let scndCareerPstn = document.querySelector('.scnd-career-pstn')
+let scndCareerDscrptn = document.querySelector('.scnd-career-dscrptn')
+
 
 // define language using window hash
 if (window.location.hash) {
@@ -38,6 +91,27 @@ if (window.location.hash) {
         contacts.textContent = locale.ru.contacts;
         headerDescription.textContent = locale.ru.about;
         description.textContent = locale.ru.infoDescription;
+        education.textContent = locale.ru.education;
+        educationDescription.textContent = locale.ru.educationDescription;
+
+        courses.textContent = locale.ru.courses;
+        englishCourses.textContent = locale.ru.englishCourses;
+        englishCoursesDescription.textContent = locale.ru.englishCoursesDescription;
+        onlineCourses.textContent = locale.ru.onlineCourses;
+        onlineCoursesDescription.textContent = locale.ru.onlineCoursesDescription;
+        technologiesTranslation.textContent = locale.ru.technologiesTranslation;
+        projectHeaderTranslation.textContent = locale.ru.projectHeaderTranslation;
+
+        careerHeaderTranslation.textContent = locale.ru.careerHeaderTranslation;
+        fsrtCareerDate.textContent = locale.ru.fsrtCareerDate;
+        fsrtCareerName.textContent = locale.ru.fsrtCareerName;
+        fsrtCareerPstn.textContent = locale.ru.fsrtCareerPstn;
+        fsrtCareerDscrptn.textContent = locale.ru.fsrtCareerDscrptn;
+
+
+        scndCareerDate.textContent = locale.ru.scndCareerDate;
+        scndCareerName.textContent = locale.ru.scndCareerName;
+        scndCareerDscrptn.textContent = locale.ru.scndCareerDscrptn;
     }
 }
 
